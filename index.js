292867473db01695ea5585aa61e854cacb6d4658
CameronBrenton <Cameron.Brenton@keyin.com>
2021-06-07 18:52:03 -0230

    Added database. Imported queries.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const db = require('./queries');
 
-let port = 3000;
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (request, response) => {
+    response.json({ message: "Eureka it worked!" });
+});
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`);
 });
